38a77c2bd197f843ea1c851e6865406a92d896a3
mengCareers <meng.careers@gmail.com>
2018-06-23 10:21:51 +0800

    C

---
+++ src/Google/IslandPerimeter.java
@@ -0,0 +1,14 @@
+package Google;
+
+/**
+ * 1 : land
+ * 0 : water
+ * one island only
+ * output : perimeter of the island
+ * edges of the water
+ */
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        
+    }
+}
